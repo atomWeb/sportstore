1de1108b6d451a75dd6e118af57e94ebadefa906
atomWeb <cristianjose@gmail.com>
2022-07-10 11:05:36 +0200

    apply the RESTful data source

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { ProductRepository } from './product.repository';
 import { StaticDataSource } from './static.datasource';
 import { Cart } from './cart.model';
 import { Order } from './order.model';
 import { OrderRepository } from './order.repository';
+import { RestDataSource } from './rest.datasource';
 
 @NgModule({
+  imports: [HttpClientModule],
   providers: [
     ProductRepository,
     StaticDataSource,
     Cart,
     Order,
     OrderRepository,
+    { provide: StaticDataSource, useClass: RestDataSource },
   ],
 })
 export class ModelModule {}
